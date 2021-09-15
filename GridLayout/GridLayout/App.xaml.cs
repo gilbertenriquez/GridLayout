ea3aead08908ba496998d50a964d0b8a17be403f
Gilbert Enriquez <gilbertenriquez10@gmail.com>
2021-09-15 09:58:06 +0800

    Add project files.

---
+++ GridLayout/GridLayout/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            //same as stacklayout change the MainPage to GridLayout if you want to run the exercise 2!
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
